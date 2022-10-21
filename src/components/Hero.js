0255960a047733fc6866931ae7717828a0262cdd
Chazz <czcarrizales@yahoo.com>
2022-10-20 17:08:20 -0700

    create hero component and css

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,17 @@
 import React from "react";
+import '../css/Hero.css'
+import chazzHeroPic from '../images/chazz-portfolio-picture.jpg'
 
 export default function Hero() {
     return (
-        <div>
-            hero
+        <div className="hero-container">
+            <div className="hero-img-container">
+                <img className="hero-img" src={chazzHeroPic}></img>
+                <p>Chazz Carrizales</p>
+            </div>
+            <div className="hero-text">
+                <p>Bunch of text here describing myself and showing why I should be hired and such. Second sentence to really sell myself and all that? Maybe a third sentence, I'm not entirely sure to be honest.</p>
+            </div>
         </div>
     )
 }
