977f8b44e0ec5850f1e7afe99f90b3a25feba50f
Chazz <czcarrizales@yahoo.com>
2022-10-20 18:48:47 -0700

    experiment with colors on hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,7 +10,7 @@ export default function Hero() {
                 <p>Chazz Carrizales</p>
             </div>
             <div className="hero-text">
-                <h2>Making Complex Apps Simple</h2>
+                <h2>Making Apps With <span className="complex">Determination</span> And <span className="simple">Perseverance</span></h2>
                 <p>Bunch of text here describing myself and showing why I should be hired and such. Second sentence to really sell myself and all that? Maybe a third sentence, I'm not entirely sure to be honest.</p>
             </div>
         </div>
