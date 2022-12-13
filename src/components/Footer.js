b539ab9e46f79658419de036b4e7d4874683662f
Chazz <czcarrizales@yahoo.com>
2022-12-12 18:04:12 -0800

    adding linkedin to footer and increasing footer height

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,7 @@ export default function Footer() {
         <div className="footer-container">
             <div className="footer-socials">
             <a href="https://github.com/czcarrizales"><i class="fa-brands fa-github fa-2x"></i></a>
+            <a href="https://www.linkedin.com/in/chazz-carrizales/"><i class="fa-brands fa-linkedin fa-2x"></i></a>
             </div>
         </div>
     )
