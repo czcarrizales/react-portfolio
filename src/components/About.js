cd4298ffd3bc5bd80f4f90765eb6a1d216c5a6df
Chazz <czcarrizales@yahoo.com>
2022-12-12 16:28:27 -0800

    change h3 to h2 (About.js)

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import '../css/About.css'
 export default function About() {
     return (
         <div className="about-container">
-            <h3>ABOUT ME</h3>
+            <h2>ABOUT ME</h2>
             <p>This is where I'm going to put a lot of content about me. This is where I kind of go through a sort of journey and such about how I got started and why I'm doing this. This is when I transition towards what I'm currently up to now. Maybe even say some interests and hobbies I do in my free time? And yeah, I'm always ready to be challenged or whatever.</p>
         </div>
     )
