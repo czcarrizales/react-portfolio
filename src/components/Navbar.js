61fb0088ff742ce54894d35c8518daecf3c67c80
Chazz <czcarrizales@yahoo.com>
2023-02-14 17:17:36 -0800

    Creating Links for navbar, and commenting out navbar temporarily

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from 'react-router-dom'
 import "../css/Navbar.css";
 import "../images/chazz-logo.png";
 import chazzLogo from "../images/chazz-logo.png";
@@ -6,20 +7,20 @@ import chazzLogo from "../images/chazz-logo.png";
 export default function Navbar() {
   return (
     <div className="navbar-container">
-      <a href="#" className="logo">
+      <Link to="/" className="logo">
         Chazz
-      </a>
+      </Link>
       <nav>
         <ul className="navLinks">
-          <li>
+          {/* <li>
             <a href="#">About</a>
-          </li>
-          <li>
+          </li> */}
+          {/* <li>
             <a href="#">Articles</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
+          </li> */}
+          {/* <li>
+            <Link to="/contact">Contact</Link>
+          </li> */}
         </ul>
       </nav>
     </div>
