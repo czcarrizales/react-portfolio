0384c1181482eeb0d231c625c551ead414947766
Chazz <czcarrizales@yahoo.com>
2023-02-14 16:58:49 -0800

    adding new images and github links to projects

--- src/components/App.js
+++ src/components/App.js
@@ -10,22 +10,20 @@ function App() {
     {
       title: "Burning Letters",
       text: "Create a temporary message and share it with the world to see.",
-      image: "https://images.unsplash.com/photo-1532045974417-fe87b46db62f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+      image: "images/burningletter.png",
+      githubLink: "https://github.com/czcarrizales/burning-letters-project"
     },
     {
-      title: "Recipe App",
-      text: "Allows users to store recipes.",
-      image: "https://images.unsplash.com/photo-1556909211-36987daf7b4d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+      title: "Quiz App",
+      text: "Create your own quizzes, and play other quizzes!",
+      image: "images/quizgame.png",
+      githubLink: "https://github.com/czcarrizales/chazz-quiz-project"
     },
     {
-      title: "Game App",
-      text: "Choose your own adventure type of game.",
-      image: "https://images.unsplash.com/photo-1579373903781-fd5c0c30c4cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
-    },
-    {
-      title: "Music Blog",
-      text: "Blog about different types of music where users are able to comment freely.",
-      image: "https://images.unsplash.com/photo-1511379938547-c1f69419868d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+      title: "Tamagotchi Clone (In Progress)",
+      text: "Adopt, play with, and feed your pet! You can even see other pets put up for adoption by other users!",
+      image: "images/tamagotchi-clone.png",
+      githubLink: "https://github.com/czcarrizales/tamagotchi-clone"
     },
   ];
   return (
@@ -36,7 +34,7 @@ function App() {
       <h2 className="projects">PROJECTS</h2>
       <div className="portfolio-container">
         {portfolio.map((p) => {
-          return <Showcase title={p.title} text={p.text} image={p.image} />;
+          return <Showcase title={p.title} text={p.text} image={p.image} githubLink={p.githubLink} />;
         })}
       </div>
       <Footer />
