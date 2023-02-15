3b2d7a32cde1aa0728c1afa2c1217449a490342b
Chazz <czcarrizales@yahoo.com>
2023-02-14 17:17:13 -0800

    creating Contact component and css file

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from "react";
+import '../css/Contact.css'
+
+export default function Contact() {
+    return (
+        <div className="contact-container">
+            <h1>CONTACT ME</h1>
+            <h2>email: czcarrizales@yahoo.com</h2>
+        </div>
+    )
+}
