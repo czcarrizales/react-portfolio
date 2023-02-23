5e4c86aa4aebab17404b07b55cb6c8cb4bf3b4a8
Chazz <czcarrizales@yahoo.com>
2023-02-23 14:16:49 -0800

    trying to brainstorm about text

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,7 @@ export default function About() {
     return (
         <div className="about-container">
             <h2>ABOUT ME</h2>
+            <p>Creativity and logic. My life has always centered around these two ideas. </p>
             <p>I cried over not getting a computer when I was younger, and from then on I knew I was destined to be heavily involved with computers in some shape or form. Ever since I was a child, my eyes would always light up when I saw some inspiring art piece, whether that be through paintings, drawings, writing, or even web applications. Once I realized that I could scratch that creative itch with web development, I decided to sit down and put all I could into learning this craft. 25 years late, of course.</p>
         </div>
     )
