e267157229ea491202d17071afae5185989a6c68
Chazz <czcarrizales@yahoo.com>
2023-02-23 15:29:51 -0800

    putting showcase title under image

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -4,8 +4,8 @@ import '../css/Showcase.css';
 export default function Showcase(props) {
     return (
         <div className="showcase-container">
-            <h3>{props.title}</h3>
             <img src={props.image}></img>
+            <h3>{props.title}</h3>
             <p>{props.text}</p>
             <a className="link" href={props.websiteLink}>Visit</a>
             <a href={props.githubLink} className="link githubLink">Github Link</a>
