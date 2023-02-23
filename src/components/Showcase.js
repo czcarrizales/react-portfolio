7658009224d2df6dcb75b6fca72f13574bd0ee49
Chazz <czcarrizales@yahoo.com>
2023-02-23 15:36:20 -0800

    adding div for showcase-links

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -7,8 +7,11 @@ export default function Showcase(props) {
             <img src={props.image}></img>
             <h3>{props.title}</h3>
             <p>{props.text}</p>
+            <div className="showcase-links">
             <a className="link" href={props.websiteLink}>Visit</a>
             <a href={props.githubLink} className="link githubLink">Github Link</a>
+            </div>
+            
         </div>
     )
 }
