3dab66cb5f0a67e9a2726d9d318cdb6597c22c0d
Chazz <czcarrizales@yahoo.com>
2023-02-23 14:17:15 -0800

    brainstorm with hero text

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,7 +11,7 @@ export default function Hero() {
             </div>
             <div className="hero-text">
                 {/* <h2>Making Apps With <span className="complex">Determination</span> And <span className="simple">Perseverance</span></h2> */}
-                <p>Full Stack web developer based in California. Over three years of experience creating applications. MEAN and MERN stacks are my speciality.</p>
+                <p>I'm a software developer based in California. I strive to bring out the best in myself, my work, and my team. With over three years of experience creating applications, I'm ready to do whatever it takes to make sure your projects come out in stellar shape.</p>
                 <div className="icons">
                 <i class="fa-brands fa-angular fa-2x"></i>
                 <i class="fa-brands fa-react fa-2x"></i>
