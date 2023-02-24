3f09c4b4c49a8e5e9084b81c8e5725df62591ff1
Chazz <czcarrizales@yahoo.com>
2023-02-23 18:09:55 -0800

    fixing up image responsiveness

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -4,7 +4,10 @@ import '../css/Showcase.css';
 export default function Showcase(props) {
     return (
         <div className="showcase-container">
+            <div className="image-container">
             <img src={props.image}></img>
+            </div>
+            
             <h3>{props.title}</h3>
             <p>{props.text}</p>
             <div className="showcase-links">
