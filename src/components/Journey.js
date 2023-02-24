2552b92a4f0b46472ec123362027efb8e6cc13bb
Chazz <czcarrizales@yahoo.com>
2023-02-23 17:52:14 -0800

    creating an essay (in progress)

---
+++ src/components/Journey.js
@@ -0,0 +1,22 @@
+import React from "react";
+import '../css/Journey.css'
+
+export default function Journey() {
+    return (
+        <div className="journey-container">
+            <h1>My Journey</h1>
+            <h2>Childhood</h2>
+            <p>Ever since I was a child, I've always had my face buried in some type of technology. Computers, TVs, game consoles, and everything in between. My family always told me that I had a future in tech, and while it seemed exciting, I never really took it seriously.</p>
+            <h2>Too Many Years Later...</h2>
+            <p>Even with the thousands of hours of experience addicted to games and phone apps, I still never truly applied myself in creating something with technology, whether that be a game, website, or any kind of software, though I dabbled in it here and there. Life is just like that sometimes.</p>
+            <h2>What am I doing?</h2>
+            <p>After years of a seemingly neverending onslaught of issues, including the infamous COVID outbreak, I decided that I needed to do something different. I paid and signed up for a series of classes that would teach you the basics of HTML all the way up to learning React. Even though I was still unsure of my future, I knew that these steps were going to be important.</p>
+            <h2>Apprenticeship</h2>
+            <p>Before I was to begin taking my JavaScript class, I was offered an apprenticeship, and from then on... I still didn't really take myself seriously.</p>
+            <h2>Something Clicked...</h2>
+            <p>After the apprenticeship, and some time just mulling around, something in my brain clicked. I was focusing more, working more, becoming more passionate about learning, and putting myself out there in ways that I was never comfortable with. It felt like a snowball effect.</p>
+            <h2>What now?</h2>
+            <p>Now, I'm just currently in the process of showing everyone of what I'm capable of. Not just with programming and designing, but in being a team player, and raising people's spirits.</p>
+        </div>
+    )
+}
