70e53a769f0ab03a18cbf1f80e60e54a3cb6f518
Chazz <czcarrizales@yahoo.com>
2023-02-24 14:42:30 -0800

    About update

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ export default function About() {
     return (
         <div className="about-container">
             <h2>ABOUT ME</h2>
-            <p>I'm a software developer based in California with experience working with MERN and MEAN stacks for website development. I am constantly looking at other's designs for inspiration, both with a frontend and backend mindset. Knowledge is key, and I'm always striving to learn about everything I can, whether it's code or non-code related. When I'm not an inch away from my computer screen hammering away at a project, I like to relax by listening to music with my noise-cancelling headphones or reading something that will teach me to be a more open-minded and fuller person.</p>
+            <p>I'm a software developer based in California with experience working with MERN and MEAN stacks for website development. I am constantly looking at other's designs for inspiration, both with a frontend and backend mindset. Knowledge is key, and I'm always striving to learn about everything I can, whether it's code or non-code related. When I'm not an inch away from my computer screen hammering away at a project, I like to relax by listening to music with my noise-cancelling headphones or reading something that will teach me to be a more open-minded and fuller person. I love talking to people and learning gaining different perspectives, so feel free to reach out anytime through my email or Linkedin!</p>
         </div>
     )
 }
